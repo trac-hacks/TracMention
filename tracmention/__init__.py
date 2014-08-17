055c999f09d08cfad83cdaaadd58baa45bb86788
Jellyfrog <Jellyfrog@users.noreply.github.com>
2014-08-17 20:29:45 +0200

    Add permission support

--- tracmention/__init__.py
+++ tracmention/__init__.py
@@ -3,36 +3,45 @@ import re
 from trac.core import *
 from trac.ticket.api import ITicketChangeListener
 from trac.ticket.notification import TicketNotifyEmail
+from trac.perm import IPermissionRequestor, PermissionSystem
 
 
 class Notifications(Component):
     """Send notification to the mentioned persons"""
-    implements(ITicketChangeListener)
+    implements(ITicketChangeListener, IPermissionRequestor)
 
     ping_regex = r'\B@(\w+)\b'
 
-    def _find_mentions(self, ticket, text, newticket=False):
+    def _find_mentions(self, ticket, text, user, newticket=False):
+        if not PermissionSystem(self.env).check_permission('MENTION_NOTIFY', user):
+            return False
+
         recipients = re.findall(self.ping_regex, text)
 
         if recipients:
             pn = PingNotifyEmail(self.env, recipients=recipients)
             pn.notify(ticket, newticket, modtime=ticket.values['changetime'])
 
+    # IPermissionRequestor method
+    def get_permission_actions(self):
+        return ['MENTION_NOTIFY']
+
+    # ITicketChangeListener methods
     def ticket_created(self, ticket):
-        self._find_mentions(ticket, ticket.values['description'], True)
+        self._find_mentions(ticket, ticket.values['description'], ticket.values['reporter'], True)
 
     def ticket_changed(self, ticket, comment, author, old_values):
         if comment:
-            self._find_mentions(ticket, comment)
+            self._find_mentions(ticket, comment, author)
 
         if 'description' in old_values:
-            self._find_mentions(ticket, ticket.values['description'])
+            self._find_mentions(ticket, ticket.values['description'], author)
 
     def ticket_deleted(self, ticket):
         pass
 
     def ticket_comment_modified(self, ticket, cdate, author, comment, old_comment):
-        self._find_mentions(ticket, comment)
+        self._find_mentions(ticket, comment, author)
         pass
 
     def ticket_change_deleted(self, ticket, cdate, changes):
